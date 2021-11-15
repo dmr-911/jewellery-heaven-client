cbb0c57a5aa727ed1bcbd11fc3c63c510b160578
mizan <dewan.mizanur911@gmail.com>
2021-11-15 09:02:36 +0600

    home page added with navbar page and banner

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import CustomNavbar from '../CustomNavbar/CustomNavbar';
+
+const Home = () => {
+    return (
+        <div>
+            <CustomNavbar></CustomNavbar>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
