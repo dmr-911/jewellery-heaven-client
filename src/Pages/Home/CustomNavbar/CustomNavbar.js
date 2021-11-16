eedc7fa38a847c9a52f0c1deced16f637b5abdfa
mizan <dewan.mizanur911@gmail.com>
2021-11-16 23:51:39 +0600

    navbar updated

--- src/Pages/Home/CustomNavbar/CustomNavbar.js
+++ src/Pages/Home/CustomNavbar/CustomNavbar.js
@@ -9,42 +9,46 @@ const CustomNavbar = () => {
     console.log(user);
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <Container fluid>
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
                     <Navbar.Brand as={Link} to="/home" className="d-flex align-items-center">
-                        <img
-                            src={logo}
-                            width="160"
-                            height="60"
-                            className="d-inline-block align-top"
-                            alt="React Bootstrap logo"
-                        />
-                        <b>Jewellery Heaven</b>
+                    <img
+                    src={logo}
+                    width="160"
+                    height="60"
+                    className="d-inline-block align-top"
+                    alt="React Bootstrap logo"
+                    />
+                    <b>Jewellery Heaven</b>
                     </Navbar.Brand>
                     <Nav className="ms-auto">
-                        <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/explore">Explore</Nav.Link>
-                        {
-                            user.email &&
-                            <Nav.Link as={NavLink} to="/dashboard">Dashboard</Nav.Link>
-                        }
-                        {
-                            user.email ?
-                            <>
-                               {
-                                   user.photoURL ? 
-                                   <img src={user?.photoURL} heigth="40" width="40" className="rounded-circle me-2" alt="dp" />
-                                   :
-                                   <span className="text-white my-auto me-2">Signed in as : {user.displayName.toUpperCase()}</span>
-                               }
-                                <Button className="btn-danger" onClick={logOut}>Logout</Button>
+                    <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/explore">Explore</Nav.Link>
+                {
+                    user.email &&
+                    <Nav.Link as={NavLink} to="/dashboard">Dashboard</Nav.Link>
+                }
+                {
+                    user.email?
+                    <>
+                {
+                    user.photoURL?
+                    <img src={user?.photoURL} heigth="40" width="40" className="rounded-circle me-2" alt="dp" />
+                    :
+                    <span className="text-white my-auto me-2">Signed in as: {user.displayName.toUpperCase()}</span>
+                }
+                    <Button className="btn-danger" onClick={logOut}>Logout</Button>
 
-                            </>
-                                :
-                                <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
-                        }
-                        {/* <p style={{ color: 'white' }}>Signed in as : {user.email}</p> */}
+                    </>
+                    :
+                    <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+                }
+                {/* <p style={{ color: 'white' }}>Signed in as : {user.email}</p> */}
                     </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
