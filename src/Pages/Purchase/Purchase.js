b89e94f4084a22161f0d6e71d03d49132434a8fc
mizan <dewan.mizanur911@gmail.com>
2021-11-16 10:37:26 +0600

    purchase field added and updated

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,11 +1,88 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
+import './Purchase.css';
+import useAuth from '../../hooks/useAuth';
+import { Modal, Button } from 'react-bootstrap';
 
 const Purchase = () => {
-    const {key} = useParams();
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+        setShow(false);
+        history.push('/');
+    };
+    const handleShow = () => setShow(true);
+    const { user } = useAuth();
+    const history = useHistory();
+    const date = new Date();
+    const { key } = useParams();
+    const [products, setProducts] = useState([]);
+    const initialInfo = { buyerName: user.displayName, email: user.email, phone: '' };
+    const [purchaseInfo, setPurchaseInfo] = useState(initialInfo);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    const matchedItem = products.find(product => product.key === parseInt(key));
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newInfo = { ...purchaseInfo };
+        newInfo[field] = value;
+        setPurchaseInfo(newInfo);
+    }
+
+    const handlePurchase = e => {
+        // collect data
+        const order = {
+            ...purchaseInfo,
+            product: matchedItem.name,
+            price: matchedItem.price - matchedItem.price * (matchedItem.offer / 100),
+            date: date.toLocaleDateString(),
+            rating: matchedItem.rating
+        }
+        // send to the server
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    handleShow();
+                }
+            });
+
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <h2>This is purchase field for ${key} product</h2>
+            <h5 className="mt-3">User Details</h5>
+            <div className="divider bg-info rounded mb-3 mx-auto"></div>
+            <form onSubmit={handlePurchase}>
+                <input name="name" defaultValue={user.displayName} type="text" className="purchase-input" onBlur={handleOnBlur} placeholder="Name" />
+                <input name="email" defaultValue={user.email} type="email" className="purchase-input" onBlur={handleOnBlur} placeholder="email" />
+                <input name="phone" type="number" className="purchase-input" onBlur={handleOnBlur} placeholder="Phone" />
+                <input name="city" type="text" className="purchase-input" onBlur={handleOnBlur} placeholder="City" />
+                <input name="country" type="text" className="purchase-input" onBlur={handleOnBlur} placeholder="Country" />
+                <input type="submit" value="Submit" className="purchase-input btn-danger" />
+            </form>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Body>Order Placed Successfully!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
