a8a09a2ba87f911caa1c19922381b99852d10889
mizan <dewan.mizanur911@gmail.com>
2022-01-11 07:54:57 +0600

    home page products modified

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -13,11 +13,11 @@ const Products = () => {
         <Container fluid  className="products">
         <Container>
             <h2 className="text-white mb-2">
-                <span className="text-white">Stocked</span>{" "}
+                <span className="text-dark">Stocked</span>{" "}
                 <span className="text-danger">Products</span>
             </h2>
             <div className="divider bg-info rounded mb-3 mx-auto"></div>
-            <Row xs={1} md={2} className="g-4 pb-5">
+            <Row xs={1} md={3} className="g-4 pb-5">
                 {
                     products.map(product => <Product
                         key={product._id}
